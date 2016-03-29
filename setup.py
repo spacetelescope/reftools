0add37ab154d6b9f71c478ba8a03cbdf68caa4d2
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-28 22:30:07 -0400

    Implement RECON

--- setup.py
+++ setup.py
@@ -1,15 +1,62 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'lib/reftools')
+
 
 setup(
-    setup_requires=['d2to1>=0.2.9', 'stsci.distutils>=0.3.2'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'reftools',
+    version = version.pep386,
+    author = 'Warren Hack, Nadezhda Dencheva, Vicki Laidler, Matt Davis, Megan Sosey, Pey Lian Lim, Mihai Cara',
+    author_email = 'help@stsci.edu',
+    description = 'Set of tools used to support creation of calibration reference files for Hubble Space Telescope',
+    url = 'https://github.com/spacetelescope/reftools',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'astropy',
+        'calcos',
+        'nose',
+        'numpy',
+        'scipy',
+        'sphinx',
+        'stsci.imagestats',
+        'stsci.sphinxext',
+        'stsci.tools',
+        'stwcs'
+    ],
+
+    package_dir = {
+        '':'lib'
+    },
+    packages = find_packages('lib'),
+    package_data = {
+        'reftools': [
+            'data/*',
+            'pars/*',
+            '*.help',
+            '*.rules',
+            'LICENSE.txt',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'tdspysyn=reftools.tdspysyn:main'
+        ]
+    },
+    ext_modules=[
+        Extension('reftools._computephotpars',
+            glob('src/*.c')
+        )
+    ]
 )
