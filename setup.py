d6c7ea437cada91ec00c1a8cb2cc5c7ee8204eee
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-09-06 23:43:20 -0400

    MNT: Replace relic with setuptools_scm

--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
 #!/usr/bin/env python
-import os
-import pkgutil
-import sys
 from glob import glob
 from setuptools import setup, Extension
-from subprocess import check_call, CalledProcessError
-
-
-if not pkgutil.find_loader('relic'):
-    relic_local = os.path.exists('relic')
-    relic_submodule = (relic_local and
-                       os.path.exists('.gitmodules') and
-                       not os.listdir('relic'))
-    try:
-        if relic_submodule:
-            check_call(['git', 'submodule', 'update', '--init', '--recursive'])
-        elif not relic_local:
-            check_call(['git', 'clone', 'https://github.com/spacetelescope/relic.git'])
-
-        sys.path.insert(1, 'relic')
-    except CalledProcessError as e:
-        print(e)
-        exit(1)
-
-import relic.release
-
-version = relic.release.get_info()
-relic.release.write_template(version, 'reftools')
 
 setup(
     name='reftools',
-    version=version.pep386,
+    use_scm_version=True,
     author=('Warren Hack, Nadezhda Dencheva, Vicki Laidler, Matt Davis, '
             'Megan Sosey, Pey Lian Lim, Mihai Cara'),
     author_email='help@stsci.edu',
@@ -46,6 +20,7 @@ setup(
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    setup_requires=['setuptools_scm'],
     install_requires=[
         'astropy>=2',
         'numpy'
