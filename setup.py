5f93fd1993dbc91f52f994ae181c2738d705fa6d
zacharyburnett <zburnett@stsci.edu>
2026-07-06 13:12:59 -0400

    [SCSB-237] build Stable ABI3 wheels with `Py_LIMITED_API`

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@
 from glob import glob
 from setuptools import setup, Extension
 
+MACROS = [
+    ("Py_LIMITED_API", 0x03090000),  # PY_VERSION_HEX for 3.11
+]
+
 setup(
-    use_scm_version={'write_to': 'reftools/version.py'},
+    use_scm_version={"write_to": "reftools/version.py"},
     ext_modules=[
-        Extension('reftools._computephotpars', glob('reftools/src/*.c'))
-    ]
+        Extension(
+            "reftools._computephotpars", glob("reftools/src/*.c"), define_macros=MACROS
+        )
+    ],
+    options={'bdist_wheel': {'py_limited_api': 'cp39'}},
 )
